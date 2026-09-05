c98dd5293f74657a4c5763f1e7f91db9d4be5f8d
ocfrey <ocfrey270@gmail.com>
2026-09-05 11:32:39 -0600

    Destructible Wall Destructs

---
+++ Assets/Scripts/BreakableWall.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableWall : AntInteractable
+{
+    private List<Ant> antsInteracting;
+    [SerializeField] private int antsNeeded;
+    private bool destroying = false;
+
+    void Start()
+    {
+        antsInteracting = new();
+    }
+    public override void AntBeginInteract(Ant ant)
+    { 
+        if (!antsInteracting.Contains(ant))
+        {
+            antsInteracting.Add(ant);
+                    if (antsInteracting.Count >= antsNeeded)
+                    {
+                        destroying = true;
+                        for (int i = antsInteracting.Count - 1; i >= 0; i--)
+                        {
+                            AntEndInteract(antsInteracting[i]);
+                        }
+                        Destroy(this.gameObject);
+                    }
+        }
+    }
+
+    public override bool CanAntInteract(Ant ant)
+    {
+        return !destroying;
+    }
+
+    public override void AntEndInteract(Ant ant)
+    {
+        antsInteracting.Remove(ant);
+    }
+}
