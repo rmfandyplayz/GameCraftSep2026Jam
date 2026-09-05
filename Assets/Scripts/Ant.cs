1e41d19b3797011d1a9a8aa83ee8046aec70727d
MysticTortoise <shadowknight7788@gmail.com>
2026-09-04 22:27:21 -0600

    ants can be directed

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -1,31 +1,67 @@
+using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Ant : MonoBehaviour
 {
-    [SerializeField] private int hunger;
-    [SerializeField] private Vector3 currentDirectedPos;
-    [SerializeField] private float moveSpeed;
+    [FormerlySerializedAs("hunger")] [SerializeField] private int Hunger;
+    private Vector3 currentDirectedPos;
+    [SerializeField] private float Acceleration;
+    [FormerlySerializedAs("moveSpeed")] [SerializeField] private float MoveSpeed;
+
+    private Rigidbody rb;
     private AntNest nest;
-    void Direct(Vector3 pos)
+    
+    public void Direct(Vector3 pos)
     {
         currentDirectedPos = pos;
-        StartCoroutine(MoveToDirectedPos(currentDirectedPos));
     }
 
-    private IEnumerator MoveToDirectedPos(Vector3 goal)
+    private bool CloseToTarget(Vector3 target)
     {
-        while (transform.position != goal)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, goal, moveSpeed);
-        }
-        yield return null;
+        return (transform.position - target).magnitude < (MoveSpeed * Time.deltaTime * 2);
     }
 
-    void Start()
+    private void MoveTowards(Vector3 target)
+    {
+        if (CloseToTarget(target))
+            return;
+
+        Vector3 accel = (target - transform.position).normalized * (Acceleration * Time.deltaTime);
+        accel.y = 0;
+        
+        rb.AddForce(accel);
+        
+        // Clamp max speed
+        Vector3 horizVel = rb.linearVelocity;
+        horizVel.y = 0;
+        if (horizVel.magnitude > MoveSpeed)
+        {
+            horizVel = horizVel.normalized * MoveSpeed;
+        }
+        rb.linearVelocity = new Vector3(horizVel.x, rb.linearVelocity.y, horizVel.z);
+    }
+
+    private void Start()
+    {
+        currentDirectedPos = transform.position;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        MoveTowards(currentDirectedPos);
+    }
+
+    private void OnEnable()
     {
         nest = FindAnyObjectByType<AntNest>();
         nest.AddAnt(this);
     }
 
+    private void OnDisable()
+    {
+        nest.RemoveAnt(this);
+    }
 }
