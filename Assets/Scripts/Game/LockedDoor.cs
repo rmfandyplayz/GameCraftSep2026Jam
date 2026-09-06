2d4dac3b9498cbe21370c7872a2dda9a301d18a5
ocfrey <ocfrey270@gmail.com>
2026-09-06 16:08:42 -0600

    SFX added

--- Assets/Scripts/Game/LockedDoor.cs
+++ Assets/Scripts/Game/LockedDoor.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class LockedDoor : MonoBehaviour
 {
+    private AudioSource jingle;
+
+    void Start()
+    {
+        jingle = GetComponent<AudioSource>();
+    }
     public void Unlock()
     {
+        jingle.Play()
         Destroy(this.gameObject);
     }
 }
