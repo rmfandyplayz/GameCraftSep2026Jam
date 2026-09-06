109b34ceed9aa95d220d06c4a95dd666f5179d4c
ocfrey <ocfrey270@gmail.com>
2026-09-05 22:05:15 -0600

    level mechanism works

--- Assets/Scripts/Game/KeyLargeCarriable.cs
+++ Assets/Scripts/Game/KeyLargeCarriable.cs
@@ -5,6 +5,7 @@ public class KeyLargeCarriable : AntLargeCarriableObject
     [SerializeField] private LockedDoor connectedDoor;
     protected override void DepositToNest(AntNest nest)
     {
+        Debug.Log("we made it back");
         connectedDoor.Unlock();
     }
 
